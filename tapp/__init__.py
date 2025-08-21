d4f79150b38a2c892d5c395c0eebbc3a2446f5a1
Hang Luo <haaaatcher@gmail.com>
2025-08-21 20:04:36 +0800

    v0.0.1:初始化项目

---
+++ tapp/__init__.py
@@ -0,0 +1,11 @@
+from .model import MoE2
+from .utils import TAPPModelInfer, TAPPException
+
+__version__ = "0.0.1"
+__author__ = "Hang Luo"
+__email__ = "haaaatcher@gmail.com"
+__all__ = [
+    "MoE2",
+    "TAPPModelInfer",
+    "TAPPException"
+]
---
+++ tapp/model.py
@@ -0,0 +1,63 @@
+import torch
+from torch import nn
+
+
+class ElementExpert(nn.Module):
+    def __init__(self, input_size: int, output_size: int, hidden_size: int, dropout_rate: float):
+        super(ElementExpert, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.BatchNorm1d(hidden_size),
+            nn.LeakyReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_size, hidden_size),
+            nn.BatchNorm1d(hidden_size),
+            nn.LeakyReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_size, output_size)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class ExpertRater(nn.Module):
+    def __init__(self, input_size: int, output_size: int, hidden_size:int, dropout_rate: float):
+        super(ExpertRater, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.BatchNorm1d(hidden_size),
+            nn.LeakyReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_size, hidden_size),
+            nn.BatchNorm1d(hidden_size),
+            nn.LeakyReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_size, output_size),
+            nn.Softmax(dim=-1)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class MoE2(nn.Module):
+    def __init__(self, element_num: int, process_num:int, property_num: int, hidden_size: int, dropout_rate: float):
+        super(MoE2, self).__init__()
+        self.element_num = element_num
+        self.process_num = process_num
+        self.experts = nn.ModuleList(
+            [ElementExpert(2 + process_num, property_num, hidden_size, dropout_rate) for _ in range(element_num - 1)]
+        )
+        self.rater = ExpertRater(element_num + process_num, element_num - 1, hidden_size, dropout_rate)
+
+    def forward(self, x):
+        score = self.rater(x)
+        score = score.unsqueeze(-1)
+        expert_outputs = [
+            expert(x[:, [0, i + 1] + list(range(self.element_num, self.element_num + self.process_num))])
+            for i, expert in enumerate(self.experts)
+        ]
+        expert_outputs = torch.stack(expert_outputs, dim=-1)
+        output = expert_outputs @ score
+        return output.squeeze(-1)
